1aed41e46e8efa0d8bba5f15a6234b59f48601f2
Simon Willshre <simonwillshire@gmail.com>
2016-08-01 22:51:10 -0400

    Added BFC for later interactions

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,6 @@ extern crate nom;
 
 mod parser;
 
-// Backtick
-// An Extended Brainfuck language transpiling to Brainfuck using bfc
-/*
-    @                   Put the pointer at the specified position (This means we need to know what our current position is)
-    >5                  Move right 5 positions
-    =0                  Set the value of the current position to 0
-    ~                   Break out of the current loop or exit the program
-    ^name               Label the current position with an idenfitier, can be used later with @name f.e.
-    !`++>[<->+]`        Declare a function with enclosed backtick expression at the current position
-    ^name!`ascii`       Declare a variable with ASCII contents at the current position
-    =``                 Set the current position to the current + length memory to the contained backtick expression
-*/
-
 fn main(){
     let test = "
         >3
